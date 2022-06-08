b421032a475743be440e14a9f1bca716bfc1338f
AnastasiaSolo <103260035+AnastasiaSolo@users.noreply.github.com>
2022-06-09 00:42:52 +0300

    -

--- Task4.py
+++
@@ -1,8 +0,0 @@
-#3.Составить список простых множителей натурального числа N
-a = int(input('inter number: '))
-b = a / 2
-mn_list = list()
-while b != 0:
-    ci = b / 2
-mn_list.append(ci)
-print ()
